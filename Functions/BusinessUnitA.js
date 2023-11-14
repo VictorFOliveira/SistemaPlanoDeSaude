022b99957b00f955b8bde9e837ae938451f66716
Werbson06 <147109077+Werbson06@users.noreply.github.com>
2023-11-14 15:42:49 -0300

    Adição de Functions em "sobre nós" e "Fale conosco"

--- Functions/BusinessUnitA.js
+++ Functions/BusinessUnitA.js
@@ -21,3 +21,22 @@ function mostrarMensagem() {
     var mensagemCard = document.getElementById("mensagemCard");
     mensagemCard.style.display = "block";
 }
+
+function Fconosco(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none"){
+        document.getElementById(el).style.display = 'block';
+    } else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
+function Snos(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none"){
+        document.getElementById(el).style.display = 'block';
+    } else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
